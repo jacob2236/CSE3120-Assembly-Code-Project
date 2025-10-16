504d761da6f8d690aabb0e0fb22fe6d33fe20af4
jacob2236 <woodsjt321@gmail.com>
2025-10-16 11:51:44 -0400

    adding random milisecond generator

--- reaction-time.asm
+++ reaction-time.asm
@@ -1,28 +1,43 @@
-; reaction_intro.asm
-; 32-bit MASM (Windows) version
-; Prints the game intro message and exits.
+; reaction_intro.asm  
+; 32-bit MASM (Windows) version  
+; Prints the game intro message and exits.  
 
-.386
-.model flat, stdcall
-option casemap:none
+.386  
+.model flat, stdcall  
+option casemap:none  
 
-INCLUDE Irvine32.inc
+INCLUDE Irvine32.inc  
 
-.data
-msg db "Welcome to our reaction time game! Once you press the space bar you will be randomly given 0-3 seconds to press the space bar again and we will measure your reaction speed!", 13,10,0
-bytesWritten dd ?
+.data  
+msg db "Welcome to our reaction time game!", 13,10, \
+        "Once you press the space bar you will be randomly given 0-3 seconds to press the space bar again and we will measure your reaction speed!", 13,10,0  
+bytesWritten dd ?  
 
-.code
-main PROC
-    ; Get handle to console output
-    INVOKE GetStdHandle, STD_OUTPUT_HANDLE
-    mov ebx, eax
+randomMillis  dd  ?            ; will store the random delay (0–3000)
 
-    ; Write the message
-    INVOKE WriteConsoleA, ebx, ADDR msg, LENGTHOF msg - 1, ADDR bytesWritten, NULL
+.code  
+main PROC  
+    ; Get handle to console output  
+    INVOKE GetStdHandle, STD_OUTPUT_HANDLE  
+    mov ebx, eax  
+    ; Write the message  
+    INVOKE WriteConsoleA, ebx, ADDR msg, LENGTHOF msg - 1, ADDR bytesWritten, NULL  
 
-    ; Exit program
-    INVOKE ExitProcess, 0
-main ENDP
+    mov ecx, 3
+    ; initialize random number generator
+    call Randomize
+    ; generate a random number between 0 and 3000 (inclusive)
+    mov  eax, 3001             ; upper bound (exclusive)
+    call RandomRange           ; EAX = random number between 0 and 3000
+    mov  randomMillis, eax     ; store result
 
-END main
+    ; display the generated value, this will be removed later
+    mov  eax, randomMillis
+    call WriteDec
+    call Crlf
+
+    ; Exit program  
+    INVOKE ExitProcess, 0  
+main ENDP  
+
+END main  
